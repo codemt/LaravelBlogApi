58fc347b82c867dc8dad143d41bfac3e4311f901
mithileshtarkar <mithilesh.tarkar@ves.ac.in>
2018-08-17 19:31:30 +0530

    added update and delete API request functions.

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -103,6 +103,35 @@ class ItemsController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $validator = Validator::make($request->all(),[
+
+            'text'=>'required',
+            'body'=>'required'
+
+    ]);
+
+    if($validator->fails()){
+
+            $response = array('response'=>$validator->messages(),'success'=>false);
+
+            return $response;
+
+    }
+    else{
+
+            // find item to update into DB.
+            $item = Item::find($id);
+            $item->text = $request->input('text');
+            $item->body = $request->input('body');
+            $item->save();
+
+
+            return response()->json($item);
+
+
+    }
+
+
     }
 
     /**
@@ -114,5 +143,10 @@ class ItemsController extends Controller
     public function destroy($id)
     {
         //
+        $item = Item::find($id);
+        $item->delete();
+        $response = array('response'=>'Item Deleted','success'=>true);
+         return $response;
+
     }
 }
